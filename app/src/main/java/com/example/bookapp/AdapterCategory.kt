0f6f2c4ff31d6c8eceb7e9e5662b53849a0f30f6
DanielCamiloFierroGarcia <dacafiga1@hotmail.com>
2023-07-21 17:54:01 -0500

    Show book categories

---
+++ app/src/main/java/com/example/bookapp/AdapterCategory.kt
@@ -0,0 +1,101 @@
+package com.example.bookapp
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.bookapp.databinding.RowCategoryBinding
+import com.google.firebase.database.FirebaseDatabase
+
+class AdapterCategory: Adapter<AdapterCategory.HolderCategory>,Filterable {
+
+    private lateinit var binding: RowCategoryBinding
+    private val context: Context
+    var categoryArrayList: ArrayList<ModelCategory>
+    private var filterList: ArrayList<ModelCategory>
+    private var filter: FilterCategory? = null
+
+    //constructor
+    constructor(context: Context, categoryArrayList: ArrayList<ModelCategory>) {
+        this.context = context
+        this.categoryArrayList = categoryArrayList
+        this.filterList = categoryArrayList
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HolderCategory {
+        //inflate bind row_category.xml
+        binding = RowCategoryBinding.inflate(LayoutInflater.from(context), parent, false)
+        return HolderCategory(binding.root)
+    }
+
+    override fun getItemCount(): Int {
+        return categoryArrayList.size//number of items on list
+    }
+
+    override fun onBindViewHolder(holder: HolderCategory, position: Int) {
+        //Get data, set date, handle clicks etc
+        //get
+        val model = categoryArrayList[position]
+        val id = model.id
+        val category = model.category
+        val uid = model.uid
+        val timestamp = model.timestamp
+
+        //set
+        holder.categoryTv.text = category
+
+        //handle click, delete category
+        holder.deleteBtn.setOnClickListener {
+            //confirm before delete
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Delete")
+                .setMessage("Are you sure you want to delete this category?")
+                .setPositiveButton("Confirm"){a,d->
+                    Toast.makeText(context, "Deleting...", Toast.LENGTH_SHORT).show()
+                    deleteCategory(model, holder)
+                }
+                .setNegativeButton("Cancel"){a,d->
+                    a.dismiss()
+                }
+                .show()
+        }
+    }
+
+    private fun deleteCategory(model: ModelCategory, holder: AdapterCategory.HolderCategory) {
+        //get id of category to delete
+        val id = model.id
+        //Firebase DB > Categories > categoryId
+        val ref = FirebaseDatabase.getInstance().getReference("Categories")
+        ref.child(id)
+            .removeValue()
+            .addOnSuccessListener {
+                Toast.makeText(context, "Deleted successfully", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {e->
+                Toast.makeText(context, "Unable to delete due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    //viewholder class to hold/init UI views for row_category
+    inner class HolderCategory(itemView: View):RecyclerView.ViewHolder(itemView){
+        var categoryTv: TextView = binding.categoryTv
+        var deleteBtn: ImageButton = binding.deleteBtn
+    }
+
+    override fun getFilter(): Filter {
+        if(filter == null){
+            filter = FilterCategory(filterList, this)
+        }
+        return filter as FilterCategory
+    }
+
+}
--- app/src/main/java/com/example/bookapp/DashboardAdminActivity.kt
+++ app/src/main/java/com/example/bookapp/DashboardAdminActivity.kt
@@ -3,14 +3,28 @@ package com.example.bookapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
 import com.example.bookapp.databinding.ActivityDashboardAdminBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.util.logging.Logger
 
 class DashboardAdminActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDashboardAdminBinding
 
     private lateinit var firebaseAuth: FirebaseAuth
+    //arraylist to hold categories
+    private lateinit var categoryArrayList: ArrayList<ModelCategory>
+    //adapter
+    private lateinit var adapterCategory: AdapterCategory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardAdminBinding.inflate(layoutInflater)
@@ -18,6 +32,27 @@ class DashboardAdminActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+        loadCategories()
+
+        //search
+        binding.searchEt.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //called as and when user type anything
+                try{
+                    adapterCategory.filter.filter(s)
+                }catch (e: Exception){
+                    print("The error ${e.message} happened")
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
 
         binding.logoutBtn.setOnClickListener {
             firebaseAuth.signOut()
@@ -28,6 +63,33 @@ class DashboardAdminActivity : AppCompatActivity() {
         binding.addcategoryBtn.setOnClickListener {
             startActivity(Intent(this, CategoryAddActivity::class.java))
         }
+    }
+
+    private fun loadCategories() {
+        //init arraylist
+        categoryArrayList = ArrayList()
+        //get all categories from FB .. FB > categories
+        val ref = FirebaseDatabase.getInstance().getReference("Categories")
+        ref.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //clear liste before starting adding data into it
+                categoryArrayList.clear()
+                for (ds in snapshot.children){
+                    //get data as model
+                    val model = ds.getValue(ModelCategory::class.java)
+                    //add to array list
+                    categoryArrayList.add(model!!)
+                }
+                //set adapter
+                adapterCategory = AdapterCategory(this@DashboardAdminActivity, categoryArrayList)
+                //set adapter to recyclerview
+                binding.categoriesRv.adapter = adapterCategory
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
     private fun checkUser() {
