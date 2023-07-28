67199c01cfcfeaf7aadca989c7b99d750acb3a1e
DanielCamiloFierroGarcia <dacafiga1@hotmail.com>
2023-07-28 13:21:02 -0500

    Show books user

--- app/src/main/java/com/example/bookapp/DashboradUserActivity.kt
+++ app/src/main/java/com/example/bookapp/DashboradUserActivity.kt
@@ -1,16 +1,28 @@
 package com.example.bookapp
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.example.bookapp.databinding.ActivityDashboardAdminBinding
 import com.example.bookapp.databinding.ActivityDashboradUserBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class DashboradUserActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDashboradUserBinding
 
     private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var categoryArrayList: ArrayList<ModelCategory>
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboradUserBinding.inflate(layoutInflater)
@@ -19,6 +31,8 @@ class DashboradUserActivity : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
 
+        setupWithViewPAgerAdapter(binding.viewPager)
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
 
         binding.logoutBtn.setOnClickListener {
             firebaseAuth.signOut()
@@ -26,6 +40,111 @@ class DashboradUserActivity : AppCompatActivity() {
             finish()
         }
 
+    }
+
+    private fun setupWithViewPAgerAdapter(viewPager: ViewPager){
+        viewPagerAdapter = ViewPagerAdapter(
+            supportFragmentManager,
+            FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT,
+            this)
+
+        categoryArrayList = ArrayList()
+        //load categories from db
+        val ref = FirebaseDatabase.getInstance().getReference("Categories")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    //clear list
+                    categoryArrayList.clear()
+                    //load some static categories ex: all, most view
+                    //add data to models
+                    val modelAll = ModelCategory("01", "All", 1, "")
+                    val modelMostViewed = ModelCategory("01", "Most Viewed", 1, "")
+                    val modelMostDownloaded = ModelCategory("01", "Most Downloaded", 1, "")
+                    //add to list
+                    categoryArrayList.add(modelAll)
+                    categoryArrayList.add(modelMostViewed)
+                    categoryArrayList.add(modelMostDownloaded)
+                    //add to viewPagerAdapter
+                    viewPagerAdapter.addFragment(
+                        BooksUserFragment.newInstance(
+                            "${modelAll.id}",
+                            "${modelAll.category}",
+                            "${modelAll.uid}"
+                        ),modelAll.category
+                    )
+                    viewPagerAdapter.addFragment(
+                        BooksUserFragment.newInstance(
+                            "${modelMostViewed.id}",
+                            "${modelMostViewed.category}",
+                            "${modelMostViewed.uid}"
+                        ),modelMostViewed.category
+                    )
+                    viewPagerAdapter.addFragment(
+                        BooksUserFragment.newInstance(
+                            "${modelMostDownloaded.id}",
+                            "${modelMostDownloaded.category}",
+                            "${modelMostDownloaded.uid}"
+                        ),modelMostDownloaded.category
+                    )
+                    //refresh list
+                    viewPagerAdapter.notifyDataSetChanged()
+                    //Now load from DB
+                    for(ds in snapshot.children){
+                        //get data in model
+                        val model = ds.getValue(ModelCategory::class.java)
+                        //add to list
+                        categoryArrayList.add(model!!)
+                        //add to viewPagerAdapter
+                        viewPagerAdapter.addFragment(
+                            BooksUserFragment.newInstance(
+                                "${model.id}",
+                                "${model.category}",
+                                "${model.uid}"
+                            ),model.category
+                        )
+                        //refresh list
+                        viewPagerAdapter.notifyDataSetChanged()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            })
+
+        //setup adapter to viewPager
+        viewPager.adapter = viewPagerAdapter
+    }
+
+    class ViewPagerAdapter(fm: FragmentManager, behaviour: Int, context: Context): FragmentPagerAdapter(fm, behaviour) {
+        //holds list of fragments ex: new instances of some fragment for each category
+        private val fragmentsList: ArrayList<BooksUserFragment> = ArrayList()
+        //list of titles of categories, for tabs
+        private val fragmentTitleList: ArrayList<String> = ArrayList()
+        private val context: Context
+
+        init {
+            this.context = context
+        }
+
+        override fun getCount(): Int {
+            return fragmentsList.size
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return fragmentsList[position]
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return fragmentTitleList[position]
+        }
+
+        fun addFragment(fragment: BooksUserFragment, title: String){
+            //add fragment thet will be passed as parameter in fragment list
+            fragmentsList.add(fragment)
+            //add title that will be passed as parameter
+            fragmentTitleList.add(title)
+        }
     }
 
     private fun checkUser() {
---
+++ app/src/main/java/com/example/bookapp/FilterPdfUser.kt
@@ -0,0 +1,48 @@
+package com.example.bookapp
+
+import android.widget.Filter
+
+class FilterPdfUser: Filter {
+    //arraylist in which we search
+    val filterList: ArrayList<ModelPdf>
+    //adapter in which filter need to be implemented
+    var adapterPdfUser: AdapterPdfUser
+
+    constructor(filterList: ArrayList<ModelPdf>, adapterPdfUser: AdapterPdfUser) : super() {
+        this.filterList = filterList
+        this.adapterPdfUser = adapterPdfUser
+    }
+
+    override fun performFiltering(constraint: CharSequence): FilterResults {
+        var const: CharSequence? = constraint
+        val results = FilterResults()
+        //value to be searched should not be null or empty
+        if(const != null && const.isNotEmpty()){
+            const = const.toString().uppercase()
+            val filteredModels = ArrayList<ModelPdf>()
+            for(i in filterList.indices){
+                //validate if match
+                if(filterList[i].title.uppercase().contains(const)){
+                    //searched value matched with title, add to list
+                    filteredModels.add(filterList[i])
+                }
+            }
+            //returnn filtered list and size
+            results.count = filteredModels.size
+            results.values = filteredModels
+        }
+        else{
+            //return original list
+            results.count = filterList.size
+            results.values = filterList
+        }
+        return results
+    }
+
+    override fun publishResults(constraint: CharSequence, results: FilterResults) {
+        //apply filter changes
+        adapterPdfUser.pdfArrayList = results.values as ArrayList<ModelPdf>
+        //notify changes
+        adapterPdfUser.notifyDataSetChanged()
+    }
+}
